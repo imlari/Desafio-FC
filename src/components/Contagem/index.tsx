7d2ccbdaa2cf12ee74b8de955c2d8167d21b8608
Larissa <96215793+imlari@users.noreply.github.com>
2023-01-20 15:51:51 -0300

    Adicionando relógio

--- src/components/Contagem/index.tsx
+++ src/components/Contagem/index.tsx
@@ -1,13 +1,15 @@
+import Botao from '../Botao'
 import style from './Contagem.module.scss'
 import Relogio from './Relogio'
 
 export default function Contagem(){
     return(
-        <div>
-            <p>Inicie a contagem</p>
-            <div>
+        <div className={style.contagem}>
+            <p className={style.titulo}>Inicie a contagem</p>
+            <div className={style.relogio}>
                 <Relogio />
             </div>
+            <Botao>Iniciar</Botao>
         </div>
     )
 }
--- src/templates/App/App.tsx
+++ src/templates/App/App.tsx
@@ -2,12 +2,14 @@ import Formulario from '../../components/Formulario';
 import Lista from '../../components/Lista';
 import style from './style.module.scss'
 import React from 'react';
+import Contagem from '../../components/Contagem';
 
 function App() {
   return (
     <div className={style.AppStyle}>
       <Formulario />
       <Lista />
+      <Contagem />
     </div>
   );
 }
